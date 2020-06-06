17e4acf86d079c4b96fe821a5dbe8e7aa236da53
Ivan Lupolov <iv.lupolov@gmail.com>
2020-06-06 23:03:18 +0300

    first commit

---
+++ src/test/java/com/lupolov/telegram/SuburbanTrainTimetableBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lupolov.telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuburbanTrainTimetableBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
