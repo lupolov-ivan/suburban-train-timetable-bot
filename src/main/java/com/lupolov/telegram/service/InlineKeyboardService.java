bdafdbbb7ade32e78039f60beb300ec0655e801d
Ivan Lupolov <iv.lupolov@gmail.com>
2020-07-04 15:44:32 +0300

    add station info to button label

--- src/main/java/com/lupolov/telegram/service/InlineKeyboardService.java
+++ src/main/java/com/lupolov/telegram/service/InlineKeyboardService.java
@@ -2,6 +2,7 @@ package com.lupolov.telegram.service;
 
 import com.lupolov.telegram.model.Station;
 import lombok.RequiredArgsConstructor;
+import org.jsoup.Jsoup;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
@@ -31,14 +32,27 @@ public class InlineKeyboardService {
 
             stations.forEach(station -> {
                 List<InlineKeyboardButton> keyboardButtonsRow = new ArrayList<>();
+                var id = station.getId();
+                var info = extractInfoText(station.getInfo());
+                var label = station.getLabel().concat(" ").concat(info);
+
                 keyboardButtonsRow.add(new InlineKeyboardButton()
-                        .setText(station.getLabel())
-                        .setCallbackData(station.getId()));
+                                          .setText(label)
+                                          .setCallbackData(id));
 
                 rowList.add(keyboardButtonsRow);
             });
 
         inlineKeyboardMarkup.setKeyboard(rowList);
         return inlineKeyboardMarkup;
+    }
+
+    private String extractInfoText(String text) {
+        var document = Jsoup.parse(text);
+        var elements = document.getElementsByTag("i");
+        var infoText = elements.get(0).wholeText();
+
+        var trimTail = infoText.substring(0,infoText.length() - 4);
+        return "("+ trimTail +".)";
     }
 }
